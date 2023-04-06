9381c5eeb85a42059b950a186e95805c78a410c3
Shaye Ellerbe <slellerbe23@gmail.com>
2023-04-05 22:45:09 -0400

    Updated array with "P"

---
+++ src/components/ProductsArray.js
@@ -0,0 +1,101 @@
+const ProductsArray = [
+    
+    {
+    "id": 1,
+    "title": "Olympic Barbell",
+    "description": "20 KG, 30MM Multi-purpose Olympic Barbell",
+    "price": "140.00",
+    "image": "../images/barbell.jpeg",
+    "category": "Weights"
+    },
+    {
+    "id": 2,
+    "title": "LB Color Bumper Plates",
+    "description": "Options: Include Complete Set or Pairs",
+    "price": "450.00",
+    "image": "../images/bumper_plates.jpeg",
+    "category": "Weights"
+    },
+    {
+    "id": 3,
+    "title": "Cast Iron Plates",
+    "description": "Options: Include Complete Set or Pairs",
+    "price": "450.00",
+    "image": "../images/cast_iron_plates.jpeg",
+    "category": "Weights"
+    },
+    {
+    "id": 4,
+    "title": "Pro Power Rack",
+    "description": "Pro Series Power Rack that can be Adjusted",
+    "price": "1399.00",
+    "image": "../images/power_rack.jpeg",
+    "category": "Machines"
+    },
+    {
+    "id": 5,
+    "title": "Belt Squat Machine",
+    "description": "Easy Set Up For a Great Leg Pump Any Day",
+    "price": "990.00",
+    "image": "../images/belt_squat.png",
+    "category": "Machines"
+    },
+    {
+    "id": 6,
+    "title": "Rubber Hex Dumbbells",
+    "description": "Options: Purchase Full Set 5 - 25 LB or Pairs",
+    "price": "1099.00",
+    "image": "../images/dumbbells.jpeg",
+    "category": "Weights"
+    },
+    {
+    "id": 7,
+    "title": "Flat Weight Bench",
+    "description": "Perfect Addition to Complete Your Home Gym",
+    "price": "150.00",
+    "image": "../images/flat_bench.jpeg",
+    "category": "Accessories"
+    },
+    {
+    "id": 8,
+    "title": "Leg Press Hack Squat",
+    "description": "This is the eighth product",
+    "price": "1890.00",
+    "image": "../images/leg_press.jpeg",
+    "category": "Machines"
+    },
+    {
+    "id": 9,
+    "title": "Pro Sled System",
+    "description": "Can Also Purchase Additional Attachments for a Plus Ultra Workout",
+    "price": "379.00",
+    "image": "../images/sled.jpeg",
+    "category": "Accessories"
+    },
+    {
+    "id": 10,
+    "title": "Plate Loaded Lat Tower",
+    "description": "This is the tenth product",
+    "price": "550.00",
+    "image": "../images/lat_tower.jpeg",
+    "category": "Machines"
+    },
+    {
+    "id": 11,
+    "title": "3-In-1 Foam Plyometric Box",
+    "description": "Perfect for Performing Shinobi Box Jumps, Step-ups, and More",
+    "price": "189.00",
+    "image": "../images/plyo_box.png",
+    "category": "Accessories"
+    },
+    {
+    "id": 12,
+    "title": "Muay Thai Heavy Bag",
+    "description": "Get a Sweaty Cardio session In and Practice Your Baki Skills",
+    "price": "137.00",
+    "image": "../images/punching_bag.png",
+    "category": "Accessorie"
+    }
+    ];
+
+    export default ProductsArray;
